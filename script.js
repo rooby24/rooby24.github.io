5bbfddada097d440cb06c011cf2f6f1844ba1583
rooby24 <tharmarooby.kesavan@student.tdsb.on.ca>
2025-01-14 15:20:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 * Paste your teachable machine link below
 *
 */
-const URL = "your teachable machine link goes here";
+const URL = "https://teachablemachine.withgoogle.com/models/CMBhs4EAW/";
 
 
 
